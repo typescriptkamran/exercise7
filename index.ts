b41ff53566b200d16366a48a4ea7d5cda9237bed
kamran <kamran6333@gmail.com>
2023-07-31 23:17:39 +0500

    update code with type litral

--- index.ts
+++ index.ts
@@ -4,26 +4,26 @@
 
 let additionResult = 4 + 4;
 
-console.log("Addition Result: "+ additionResult)
+console.log(`Addition Result: ${additionResult}`)
 
 
 // Subtraction 
 
 let subtractionResult = 16-8
 
-console.log("Sutracion Results: " + subtractionResult);
+console.log(`Sutracion Results: ${subtractionResult}`);
 
 // Multiplication
 
 let multiplicationResult = 4 * 2;
 
-console.log("Multiplications result: " + multiplicationResult) 
+console.log(`Multiplications result: ${multiplicationResult}`) 
 
 // Division
 
-let divisionResult = 64 / 8;
+let divisionResult = 16 / 2;
 
-console.log("Division Reslt: " + divisionResult)
+console.log(`Division Reslt: ${divisionResult}`)
 
 
 
